fae5519f11b759968438a62b7d3c6bbe781439cf
Benjamin Markintosh <benjaminokiza@gmail.com>
2024-03-07 09:57:45 +0300

    Added typescript to the ProductListItem

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,16 +1,27 @@
 import Colors from "@/src/constants/Colors";
 import { StyleSheet, Text, View, Image } from "react-native";
+import { Product } from "../types";
+// interface Product {
+//   name: string;
+//   image: string;
+//   price: number;
+// }
 
-interface Product {
-  name: string;
-  image: string;
-  price: number;
-}
+export const defaultPizzaImage =
+  "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/peperoni.png";
 
-const ProductListItem: React.FC<{ product: Product }> = ({ product }) => {
+type ProductListItemProps = {
+  product: Product;
+};
+
+const ProductListItem = ({ product }: ProductListItemProps) => {
   return (
     <View style={styles.container}>
-      <Image source={{ uri: product.image }} style={styles.image} />
+      <Image
+        source={{ uri: product.image || defaultPizzaImage }}
+        style={styles.image}
+        resizeMode="contain"
+      />
       <Text style={styles.title}>{product.name}</Text>
       <Text style={styles.price}>${product.price}</Text>
     </View>
@@ -24,6 +35,8 @@ const styles = StyleSheet.create({
     backgroundColor: "white",
     padding: 10,
     borderRadius: 20,
+    flex: 1,
+    maxWidth: "50%",
   },
   title: {
     color: "black",
